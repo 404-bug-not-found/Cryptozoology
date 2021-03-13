5ad63287dff0e87701fa537815be6df6ddb0eff9
404-bug-notfound <iqbal.jamadar@gmail.com>
2021-03-13 12:20:08 -0500

    Project initialization

---
+++ src/test/java/com/galvanize/Cryptozoology/CryptozoologyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.Cryptozoology;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptozoologyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
